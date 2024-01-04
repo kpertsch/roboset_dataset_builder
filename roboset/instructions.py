d8e750b87e94b0b23145b91f94070dfed1da6f1e
Karl Pertsch <pertsch@usc.edu>
2024-01-04 11:02:03 -0500

    add kinesthetic instructions

--- roboset/instructions.py
+++ roboset/instructions.py
@@ -1,6 +1,7 @@
 
 
 ROBOSET_INSTRUCT_MAPPINGS = {
+    # teleop data
     'slide_open_drawer': "Open the drawer.",
     'pick_butter': "Pick up the butter.",
     'place_butter': "Place the butter on the board.",
@@ -21,6 +22,31 @@ ROBOSET_INSTRUCT_MAPPINGS = {
     'serve_soup_place_bowl': "Put the bowl on the board.",
     'pick_cup': "Pick up the cup.",
     'place_cup': "Place the cup in the drawer.",
+
+    # kinesthetic data
+    'pick_banana_place_in_mug': "Pick Banana and place it in mug.",
+    'pick_banana_place_in_strainer': "Pick Banana and place it in strainer.",
+    'pick_banana_from_plate_place_on_table': "Pick banana from plate and place on table.",
+    'pick_banana_from_toaster_place_on_table': "Pick banana from oven and place on table.",
+    'pick_banana_place_on_plate': "Pick banana from table and place on plate.",
+    'pick_banana_place_on_toaster': "Pick banana from table and place on oven.",
+    'pick_ketchup_place_in_strainer': "Pick Ketchup from the table and place it in strainer.",
+    'pick_ketchup_place_in_toaster': "Pick Ketchup from the table and place in oven.",
+    'pick_ketchup_from_strainer_place_on_table': "Pick ketchup from strainer and place it on the table",
+    'pick_ketchup_from_plate_place_on_table': "Pick ketchup from plate and place it on table.",
+    'pick_ketchup_from_toaster_place_on_table': "Pick Ketchup from oven and place it on table.",
+    'pick_ketchup_place_on_plate': "Pick ketchup from table and place on plate.",
+    'pick_ketchup_place_on_toaster': "Pick ketchup from table and place on oven.",
+    'drag_mug_backward': "Drag mug backwards.",
+    'drag_mug_forward': "Drag mug forwards.",
+    'drag_mug_from_left_to_right': "Drag mug left to right.",
+    'drag_mug_from_right_to_left': "Drag mug right to left.",
+    'drag_strainer_backward': "Drag strainer backwards.",
+    'drag_strainer_forward': "Drag strainer forwards.",
+    'drag_strainer_left_to_right': "Drag strainer left to right.",
+    'drag_strainer_right_to_left': "Drag strainer right to left.",
+    'flap_open_toaster_oven': "Flap open oven.",
+    'flap_close_toaster_oven': "Flap close oven."
 }
 
 
